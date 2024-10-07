03703cda702d7c37d23a9e85c234042a42aeb7f8
chidera-collins <chideracollins24@gmail.comm>
2024-10-07 11:08:32 +0100

    'Tailwind Edit'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textc: '#ecf0f1',
+        btncolo: ' #f1c40f',
+        MainBg: '#212f3d'
+      }
+    },
   },
   plugins: [],
 }
